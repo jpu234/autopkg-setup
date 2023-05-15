78dbf1ff90d48499171cac8ab99bab954d0e1812
Casey Hutchinson <hutchster@gmail.com>
2023-05-15 10:26:25 -0400

    Update add-recipes.sh
    Sorted by repo name

--- add-recipes.sh
+++ add-recipes.sh
@@ -2,14 +2,14 @@
 
 ## Add some recipes to AutoPkg
 
-autopkg repo-add recipes
-autopkg repo-add scriptingosx-recipes
-autopkg repo-add hjuutilainen-recipes
-autopkg repo-add rtrouton-recipes
-autopkg repo-add jleggat-recipes
 autopkg repo-add brianwells-recipes
 autopkg repo-add grahampugh-recipes
 autopkg repo-add hansen-m-recipes
-autopkg repo-add jps3-recipes
+autopkg repo-add hjuutilainen-recipes
 autopkg repo-add homebysix-recipes
+autopkg repo-add jleggat-recipes
+autopkg repo-add jps3-recipes
+autopkg repo-add recipes
+autopkg repo-add rtrouton-recipes
+autopkg repo-add scriptingosx-recipes
 autopkg repo-add https://github.com/jchutc0/autopkg-uploaders
